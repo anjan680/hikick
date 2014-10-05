ef001581cbdcc09f147b02d82c44f72c9afcac6d
unknown <anjan680@gmail.com>
2014-10-05 18:36:48 +0530

    coundown module created

--- js/form.js
+++ js/form.js
@@ -1,11 +1,1 @@
-$(document).ready(function() {
-    new Picker.Date($$('.date'), {
-        timePicker: false,
-        positionOffset: {
-            x: 5,
-            y: 0
-        },
-        pickerClass: 'datepicker_dashboard',
-        useFadeInOut: !Browser.ie
-    });
-});
+
